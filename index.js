1989f052a0910fb92706d8fa489631397d9b02ab
irusaf@yahoo.com <irusaf@yahoo.com>
2018-09-01 19:50:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   
   if (string === string.toLowerCase())
-    returns 
+    return "I can\'t hear you!";
     
   else if (string === string.toUpperCase())
-    returns "YES INDEED!";
+    return "YES INDEED!";
     
   else if (string === "I love you, Grandma.")
-    returns "I love you, too.";
+    return "I love you, too.";
 }
