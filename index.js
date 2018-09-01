3c872e897a84769fff5be204bdd92bbc7db1b256
irusaf@yahoo.com <irusaf@yahoo.com>
2018-09-01 19:46:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ function sayHiToGrandma(string) {
     returns "I can\'t hear you!";
     
   else if (string === string.toUpperCase())
-    if (string === string.toLowerCase())
+    returns "YES INDEED!";
 }
